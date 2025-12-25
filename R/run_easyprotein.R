5fcc802e2d61c023f8fc3e88a853cdb6fec2b039
yuanlizhanshi <826907860@qq.com>
2025-12-25 17:00:43 +0800

    Update launch_EasyProtein

--- R/run_easyprotein.R
+++ R/run_easyprotein.R
@@ -3,11 +3,57 @@
 #' @description
 #' Launch the EasyProtein interactive Shiny application.
 #'
+#' @param install_missing Logical. Whether to install missing packages automatically.
 #' @param ... Passed to shiny::runApp().
 #'
 #' @importFrom shiny runApp
 #' @export
-launch_EasyProtein <- function(...) {
+launch_EasyProtein <- function(install_missing = TRUE, ...) {
+
+  ## --------------------------------------------------
+  ## 1. Check required Shiny-related packages
+  ## --------------------------------------------------
+  required_pkgs <- c(
+    "bslib",
+    "DT",
+    "htmltools",
+    "httr",
+    "reactable",
+    "shiny",
+    "shinyjqui",
+    "shinyjs",
+    "shinydashboard",
+    "shinythemes",
+    "shinyWidgets",
+    "ggiraph"
+  )
+
+  missing_pkgs <- required_pkgs[!vapply(
+    required_pkgs,
+    requireNamespace,
+    logical(1),
+    quietly = TRUE
+  )]
+
+  if (length(missing_pkgs) > 0) {
+    msg <- paste(
+      "The following packages are required to run the EasyProtein Shiny app:\n",
+      paste(missing_pkgs, collapse = ", ")
+    )
+
+    if (!install_missing) {
+      stop(msg, call. = FALSE)
+    }
+
+    message(msg)
+    message("Installing missing packages...")
+
+    install.packages(missing_pkgs)
+  }
+
+  ## --------------------------------------------------
+  ## 2. Locate Shiny app directory
+  ## --------------------------------------------------
   app_dir <- system.file(
     "shiny",
     "easyprotein",
@@ -17,10 +63,14 @@ launch_EasyProtein <- function(...) {
   if (app_dir == "") {
     stop(
       "EasyProtein Shiny app not found.\n",
-      "Please make sure the package is installed correctly."
+      "Please make sure the package is installed correctly.",
+      call. = FALSE
     )
   }
 
+  ## --------------------------------------------------
+  ## 3. Launch Shiny app
+  ## --------------------------------------------------
   runApp(
     appDir = app_dir,
     launch.browser = TRUE,
